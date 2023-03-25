e399a821f262c6a68f96745a05b198d78d0b8b16
NIEVES Ricardo - Consultor Externo <Ricardo.NIEVES@consulting-for.edenred.com>
2023-03-24 20:28:22 -0600

    Se carga back para cotizacion de polizas

---
+++ WpfApp1/Notifications.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Notifications
+    {
+        public string code { get; set; }
+        public string message { get; set; }
+        public DateTime timeStamp { get; set; }
+    }
+}
--- WpfApp1/quotations.xaml.cs
+++ WpfApp1/quotations.xaml.cs
@@ -15,6 +15,50 @@ using System.Windows.Shapes;
 
 namespace WpfApp1
 {
+    public class Quotation
+    {
+        public string name { get; set; }
+        public string fernando { get; set; }
+        public string lastName { get; set; }
+        public string secondLastName { get; set; }
+        public DateTime birthDate { get; set; }
+        public string email { get; set; }
+        public decimal pricing { get; set; }
+        public string brand { get; set; }
+        public string model { get; set; }
+        public string color { get; set; }
+        public DateTime soldAt { get; set; }
+        public long presaleId { get; set; }
+        public int branchId { get; set; }
+        public string externalId { get; set; }
+    }
+
+    public class data : Notifications
+    {
+        public string quotationId { get; set; }
+        public Quotations quotations { get; set; }
+    }
+
+    public class Quotations
+    {
+        public string paymentMethod { get; set; }
+        public string insuranceAmount { get; set; }
+        public string installments { get; set; }
+    }
+
+    public class BBL
+    {
+        public data SendRequest(Quotation request)
+        {
+            data response = new()
+            {
+                quotations = new Quotations()
+            };
+            return response;
+
+        }
+    }
+
     /// <summary>
     /// Interaction logic for quotations.xaml
     /// </summary>
@@ -27,11 +71,24 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            Quotation request = new Quotation
+            {
+                name = txtName.Text,
+                lastName = txtApPaterno.Text,
+                secondLastName = txtApMaterno.Text,
+                birthDate = DateTime.Parse(calDateBirth.Text),
+                email = txtEmail.Text,
+                pricing=decimal.Parse(txtPricing.Text),
+                brand=txtBrand.Text,
+                model=txtModel.Text,
+                color=txtColor.Text,
+                soldAt=DateTime.Parse(calSoldAt.Text)
+            };
+            new BBL().SendRequest(request);
         }
 
         private void calDateBirth_Loaded(object sender, RoutedEventArgs e)
-        {
+        { 
             DatePicker datePicker = new DatePicker();
             if (datePicker != null)
             {
@@ -63,5 +120,7 @@ namespace WpfApp1
 
             return null;
         }
+
+
     }
 }
